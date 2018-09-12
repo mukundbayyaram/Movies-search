12662afdad2d94c29dba5e6e3cb5993c520a8871
mukund <mukundbayyaram@gmail.com>
2018-09-12 16:04:28 +0530

    App folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResultsComponent } from './component/results/results.component';
+import { FetchService } from './services/fetch.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [FetchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
